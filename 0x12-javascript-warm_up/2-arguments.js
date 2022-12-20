ddea75fbadd73def412cae5fa1f1e656dd3ef78a
Sheyi Gaji <seezie@gmail.com>
2022-12-19 22:08:16 -0800

    Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-
+if (process.argv.length === 0) {
+  console.log('No argument');
+} else if (process.argv === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
