efbb808a1f2bea3409feee26aec1d8210b932a6f
Sheyi Gaji <seezie@gmail.com>
2022-12-19 21:33:39 -0800

    warm-up turn up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const worda = 'C is fun';
 const wordb = '\nPython is cool';
-const wordc = '\nJavascript is amazing\n';
+const wordc = '\nJavascript is amazing';
 console.log(worda, wordb, wordc);
+
